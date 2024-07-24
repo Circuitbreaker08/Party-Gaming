cb6c4420f87c3a3de473af537b06752aa8e115a4
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-07-24 16:37:44 -0400

    Added assets resources

---
+++ assets.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+
+pygame.init()
+
+sprites = {}
+
+root = os.getcwd()
+os.chdir(os.path.join(root, "assets/sprites"))
+
+for directory in os.listdir():
+    sprites.update({directory: {}})
+    os.chdir(os.path.join(root, "assets/sprites", directory))
+    for file in os.listdir():
+        sprites[directory].update({file: pygame.image.load(file).convert()})
