8c0856aeb97d18b7b29bdab4e03f0ba763ab3e81
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-07-24 18:17:44 -0400

    Updated Buttons To Stage For Game Start

--- main.py
+++ main.py
@@ -8,20 +8,48 @@ screen = pygame.display.set_mode((info.current_w, info.current_h))
 clock = pygame.time.Clock()
 
 import assets
+import connection
 
-class Button():
-    def __init__(self, position: tuple[int, int], size: tuple[int, int], color: tuple[int, int, int], func):
+
+class ButtonPrimitive():
+    def __init__(self, position: tuple[int, int], size: tuple[int, int], func):
+        self.position = position
+        self.size = size
+        self.func = func
+
+    def tick(self):
+        if mouse[0] > self.position[0] and mouse[1] > self.position[1] and mouse[0] < self.position[0] + self.size[0] and mouse[1] < self.position[1] + self.size[1] and mouse[0]:
+            self.func()
+
+
+class ButtonColor(ButtonPrimitive):
+    def __init__(self, position, size, color, func):
         self.position = position
         self.size = size
         self.color = color
         self.func = func
 
     def tick(self):
-        if mouse[0] > self.position[0] and mouse[1] > self.position[1] and mouse[0] < self.position[0] + self.size[0] and mouse[1] < self.position[1] + self.size[1] and mouse[0]:
-            self.func()
         pygame.draw.rect(screen, self.color, pygame.rect.Rect(self.position, self.size))
+        super().tick()
 
-ui: list[Button] = [Button((100, 100), (64, 64), (255, 0, 0), lambda: print("Hello World"))]
+
+class Button(ButtonPrimitive):
+    def __init__(self, position, size, sprite, func):
+        self.position = position
+        self.size = size
+        self.sprite = sprite
+        self.func = func
+        
+    def tick(self):
+        screen.blit(self.sprite, self.position)
+        super().tick()
+
+
+ui: list[Button] = [
+    Button((100, 100), (320, 64), assets.sprites["ui"]["join.png"], lambda: print("Join")),
+    Button((100, 200), (320, 64), assets.sprites["ui"]["host.png"], lambda: print("Host"))
+]
 
 running = True
 while running:
