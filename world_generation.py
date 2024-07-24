b028d4d10485cd98d8c2fbad0058ac91681eb858
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2024-07-24 18:11:58 -0400

    cheeky world gen ig

---
+++ world_generation.py
@@ -0,0 +1,30 @@
+import random
+
+class WorldGeneration():
+    def __init__(self, world_size: int = 16, chunk_size: int = 16) -> None:
+        self.chunk_size: int = chunk_size
+        self.world_size: int = world_size
+        self.world: dict = self.generate()
+    
+    def generate(self) -> dict:
+        world: dict = {}
+        
+        for x in range(self.world_size):
+            for y in range(self.world_size):
+                chunk_id: str = f"{x},{y}"
+                chunk_data: list[int] = self.generate_chunk((x,y))
+                world.update(chunk_id, chunk_data)
+
+        return world
+    
+    def generate_chunk(self, chunk_coords) -> list[int]:
+        blocks: list[int] = []
+        for x in range(self.chunk_size):
+            for y in range(self.chunk_size):
+                
+                # Checkerboard Pattern
+                y = (x + y) % 2
+                
+                blocks.append(y)
+        
+        return blocks
